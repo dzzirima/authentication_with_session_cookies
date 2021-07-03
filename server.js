b8c62ff82b42b76a8941e599714c71109c06ceb7
dzzirima@gmail.com <dzzirima@gmail.com>
2021-07-03 14:22:27 +0200

    added the session store and the table is  now being created .. woow but play aroud es6 for proper configurations

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 import express from 'express'
 import session from 'express-session'
 import mongoose from 'mongoose'
+import MongoDBSession  from 'connect-mongodb-session'
+const MYsessions = MongoDBSession(session)
+
+var dbUrl = 'mongodb://localhost:27017/sessions'
 
 const app = express()
+
+//creating a store for the soring of sessions
+const store = new MYsessions({
+    uri:dbUrl,
+    collection:"mySessions"
+
+})
+
+
+
+
+
 // creating the session midleware that is going to be invoked whenever
 // we make a request to the server
 app.use(session({
     secret:'david',// this is used to sign the all session
     resave:false,// forces the session to be resaved in the session store ev
                 // even if the session was never  changed when the request was made
-    saveUninitialized:false
+    saveUninitialized:false,
+    store:store
 }))
 // by usig this middleware node creates a session body  called req.session
 
 
-mongoose.connect('mongodb://localhost:27017/sessions',{
+mongoose.connect(dbUrl,{
     useNewUrlParser :true,
     useCreateIndex:true,
     useUnifiedTopology:true
