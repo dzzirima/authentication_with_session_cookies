2d921ef4325d54cd3681bad66a0f6968580d3da4
dzzirima@gmail.com <dzzirima@gmail.com>
2021-07-03 13:57:02 +0200

    database setup for the storage of sessions

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 import express from 'express'
 import session from 'express-session'
+import mongoose from 'mongoose'
 
 const app = express()
+// creating the session midleware that is going to be invoked whenever
+// we make a request to the server
+app.use(session({
+    secret:'david',// this is used to sign the all session
+    resave:false,// forces the session to be resaved in the session store ev
+                // even if the session was never  changed when the request was made
+    saveUninitialized:false
+}))
+// by usig this middleware node creates a session body  called req.session
 
+
+mongoose.connect('mongodb://localhost:27017/sessions',{
+    useNewUrlParser :true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+}).then(res=>console.log("Data base connected "))
 
 
 app.get("/",(req,res)=>{
+    //console.log(req)
+    // req.session.name = "David"
+    // console.log(req.session)
     res.send("Hello world session ")
 })
 
